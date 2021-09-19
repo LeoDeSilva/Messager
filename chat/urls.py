37861d655e894963ef48882427fb78b4a0b859ad
leoDesilva <46300158+leoDesilva@users.noreply.github.com>
2021-09-19 13:29:36 +0100

    html redesign

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#    path("",views.home,name="home"),
+#    path("test",views.test,name="test"),
+#    path("<str:room>/",views.room, name="room"),
+#    path("checkview", views.checkview, name="checkview"),
+#    path("send", views.send, name="send"),
+#    path("getMessages/<str:room>/",views.getMessages, name="getMessages"),
+# ]
+
 urlpatterns = [
-    path("",views.home,name="home"),
-    path("<str:room>/",views.room, name="room"),
+    path("", views.home, name="home"),
+    path("test", views.test, name="test"),
+    path("<str:room>/", views.room, name="room"),
     path("checkview", views.checkview, name="checkview"),
     path("send", views.send, name="send"),
-    path("getMessages/<str:room>/",views.getMessages, name="getMessages"),
+    path("getMessages/<str:room>/", views.getMessages, name="getMessages"),
 ]
--- chatapp/settings.py
+++ chatapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'w*zx*%=!ez*3b0-v59+h!%(pzdro3__jv9lhc8^@+v24vc+n=r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
